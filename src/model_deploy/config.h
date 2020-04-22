98bb5cb7e4f213235c9e4b14279bee512008de7e
jerry0613 <lionking4234@gmail.com>
2020-04-22 09:08:29 +0800

    test

--- src/model_deploy/config.h
+++ src/model_deploy/config.h
@@ -18,7 +18,7 @@ struct Config {
 
   // The number of expected consecutive inferences for each gesture type.
 
-  const int consecutiveInferenceThresholds[label_num] = {30, 20, 20};
+  const int consecutiveInferenceThresholds[label_num] = {20, 10, 30};
 
 
   const char* output_message[label_num] = {
